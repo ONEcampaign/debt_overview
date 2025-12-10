9765d76946040b5315493c08678720642b209dbc
Luca Picci <lpicci96@gmail.com>
2025-12-10 09:29:22 +0200

    add extraction script

---
+++ scripts/analysis/get_raw_data.py
@@ -0,0 +1,43 @@
+"""Get raw data"""
+
+from bblocks.data_importers import InternationalDebtStatistics
+
+from scripts.config import Paths
+from scripts.logger import logger
+
+import signal
+
+def timeout_30min(func):
+    """Decorator to timeout a function after 30 minutes and implement a
+    try except block to catch any exceptions raised within the function."""
+    def handler(signum, frame):
+        raise TimeoutError("Function timed out after 30 minutes")
+
+    def wrapper():
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(30 * 60)  # 30 minutes
+        try:
+            return func()
+        except Exception as e:
+            raise RuntimeError(f"Could not complete data download: {str(e)}")
+        finally:
+            signal.alarm(0)
+
+    return wrapper
+
+
+@timeout_30min
+def get_debt_stocks_data() -> None:
+    """Get the raw data for the International Debt Statistics."""
+
+    ids = InternationalDebtStatistics()
+    inds = list(ids.debt_stock_indicators.indicator_code.unique())
+
+    df = ids.get_data(inds, include_labels=True)
+    df.to_parquet(Paths.raw_data / "ids_debt_stocks.parquet", index=False)
+
+    logger.info("IDS debt stocks data downloaded successfully.")
+
+
+if __name__ == "__main__":
+    get_debt_stocks_data()
